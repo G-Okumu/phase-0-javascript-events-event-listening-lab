a17ca6982c7daa9430f37e3d9a451ccdd4c73f77
George-Okumu <gokumu12@gmail.com>
2022-03-27 00:21:42 +0300

    declared new function and added it as a callback

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+function alertMe() {
+    alert("Hurray! am really working!");
+}
+
 function addingEventListener() {
     let input = document.getElementById("input");
-    input.addEventListener('click', function() { alert("I am clicked") });
+    input.addEventListener('click', alertMe);
 }
+// const input = document.getElementById('input');
+// input.addEventListener('click', function() {
+//     alert('I was clicked!');
+// });
+// document.getElementById('input').addEventListener('click', function() {
+//     alert('I was clicked!');
+// });
+
+
+// document.getElementById('input').addEventListener('click', alertMe);
